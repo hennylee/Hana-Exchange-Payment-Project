8e9645fd4311deedb2855febc226f675f4e3356c
hennylee <haenyilee@kakao.com>
2021-07-08 17:58:55 +0900

    0708 엔티티 설계

--- code/nodejs/index.js
+++ code/nodejs/index.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
-const http = require('http')
+
 const app = express();
-const server = http.Server(app);
-const io = require('socket.io')(app);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, 'index.html'));
